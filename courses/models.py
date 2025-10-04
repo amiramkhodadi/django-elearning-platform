5bccce1691c0908e8b179eda960d0d2184626be4
Amir khodadi <amiramkhodadi@gmail.com>
2025-10-04 11:42:46 +0330

    create  polymorpism content model

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -40,3 +42,15 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(
+        Module, on_delete=models.CASCADE, related_name="contents"
+    )
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, related_name="+"
+    )
+
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey("content_type", "object_id")

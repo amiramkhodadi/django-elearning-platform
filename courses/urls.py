fb4ce727dc921e8704a6e6390ae151bbff49199d
Amir khodadi <amiramkhodadi@gmail.com>
2025-10-07 17:59:24 +0330

    add  url  to  content managment  view

--- courses/urls.py
+++ courses/urls.py
@@ -13,5 +13,19 @@ urlpatterns = [
         views.CourseModuleUpdateView.as_view(),
         name="course_module_update",
     ),
-    3,
+    path(
+        "module/<int:module_id>/content/<model_name>/create/",
+        views.ContentCreateUpdateView.as_view(),
+        name="module_content_create",
+    ),
+    path(
+        "module/<int:module_id>/content/<model_name>/<id>/",
+        views.ContentCreateUpdateView.as_view(),
+        name="module_content_update",
+    ),
+    path(
+        "content/<int:id>/delete/",
+        views.ContentDeleteView.as_view(),
+        name="module_content_delete",
+    ),
 ]

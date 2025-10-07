8326a94dc1908e1bf5ced36ef6a1cd5142f7f7fe
Amir khodadi <amiramkhodadi@gmail.com>
2025-10-07 11:38:43 +0330

    adding  custom model  filed  to  the  project

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from .fields import OrderField
+
 
 # Create your models here.
 class Subject(models.Model):
@@ -39,9 +41,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=["course"])
 
     def __str__(self):
-        return self.title
+        return f"{self.order} - {self.title}"
+
+    class Meta:
+        ordering = ["order"]
 
 
 class Content(models.Model):
@@ -53,9 +59,12 @@ class Content(models.Model):
         on_delete=models.CASCADE,
         limit_choices_to={"model__in": ("text", "video", "image", "file")},
     )
-
+    order = OrderField(blank=True, for_fields=["module"])
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
+
+    class Meta:
+        ordering = ["order"]
 
 
 class ItemBase(models.Model):

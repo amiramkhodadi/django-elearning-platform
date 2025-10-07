66e604445b1cde862816b0a661db7a5461c454cb
Amir khodadi <amiramkhodadi@gmail.com>
2025-10-07 12:38:58 +0330

    creating  view for  login log out  page

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
 
 
